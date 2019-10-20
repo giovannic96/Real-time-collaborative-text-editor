130520d2bf2e5d4fa87a5f262f9765d76752c2f6
giovannic96 <giovanni.cala@hotmail.it>
2019-10-20 10:22:15 +0200

    resolve conflicts

--- ClientModule/generated/uis/ui_startwindow.h
+++ ClientModule/generated/uis/ui_startwindow.h
@@ -28,7 +28,6 @@ public:
     QStackedWidget *stackedWidget;
     QWidget *loginPage;
     QPushButton *LoginAdmin;
-    QLabel *Seinuovo;
     QPushButton *LoginButton;
     QLineEdit *LoginPasswordForm;
     QPushButton *RegisterButton;
@@ -36,7 +35,6 @@ public:
     QLineEdit *LoginUsernameForm;
     QLabel *label_status;
     QWidget *regPage;
-    QLabel *Haigiaaccount;
     QLineEdit *RegUsernameForm;
     QLineEdit *RegMailForm;
     QPushButton *SignUpButton;
@@ -53,20 +51,20 @@ public:
     {
         if (StartWindow->objectName().isEmpty())
             StartWindow->setObjectName(QString::fromUtf8("StartWindow"));
-        StartWindow->resize(900, 525);
+        StartWindow->resize(1100, 641);
         StartWindow->setMinimumSize(QSize(900, 525));
-        StartWindow->setMaximumSize(QSize(900, 525));
+        StartWindow->setMaximumSize(QSize(1100, 641));
         centralwidget = new QWidget(StartWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         stackedWidget = new QStackedWidget(centralwidget);
         stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
-        stackedWidget->setGeometry(QRect(0, 0, 900, 525));
+        stackedWidget->setGeometry(QRect(0, 0, 1100, 641));
         loginPage = new QWidget();
         loginPage->setObjectName(QString::fromUtf8("loginPage"));
         loginPage->setStyleSheet(QString::fromUtf8("#loginPage {border-image: url(:/image/accesso.png);}"));
         LoginAdmin = new QPushButton(loginPage);
         LoginAdmin->setObjectName(QString::fromUtf8("LoginAdmin"));
-        LoginAdmin->setGeometry(QRect(510, 400, 201, 41));
+        LoginAdmin->setGeometry(QRect(860, 590, 201, 41));
         QFont font;
         font.setBold(true);
         font.setWeight(75);
@@ -83,15 +81,9 @@ public:
 "#LoginAdmin:pressed{\n"
 "background-color: #800000;\n"
 "}"));
-        Seinuovo = new QLabel(loginPage);
-        Seinuovo->setObjectName(QString::fromUtf8("Seinuovo"));
-        Seinuovo->setGeometry(QRect(650, 480, 91, 21));
-        QFont font1;
-        font1.setPointSize(12);
-        Seinuovo->setFont(font1);
         LoginButton = new QPushButton(loginPage);
         LoginButton->setObjectName(QString::fromUtf8("LoginButton"));
-        LoginButton->setGeometry(QRect(740, 310, 121, 41));
+        LoginButton->setGeometry(QRect(930, 370, 121, 41));
         LoginButton->setFont(font);
         LoginButton->setStyleSheet(QString::fromUtf8("#LoginButton{color: rgb(255, 255, 255); \n"
 "background-color: rgb(20, 115, 255); \n"
@@ -107,34 +99,41 @@ public:
 "}"));
         LoginPasswordForm = new QLineEdit(loginPage);
         LoginPasswordForm->setObjectName(QString::fromUtf8("LoginPasswordForm"));
-        LoginPasswordForm->setGeometry(QRect(641, 258, 231, 31));
+        LoginPasswordForm->setGeometry(QRect(784, 320, 231, 31));
+        QFont font1;
+        LoginPasswordForm->setFont(font1);
         LoginPasswordForm->setStyleSheet(QString::fromUtf8("background-color: transparent;\n"
 "border-radius: 15px;\n"
 "padding: 0 8px;\n"
 "selection-background-color: darkgray;\n"
-"font-size: 16px;\n"
+"font-size: 18px;\n"
 ""));
         LoginPasswordForm->setEchoMode(QLineEdit::Password);
         RegisterButton = new QPushButton(loginPage);
         RegisterButton->setObjectName(QString::fromUtf8("RegisterButton"));
-        RegisterButton->setGeometry(QRect(740, 480, 81, 23));
-        RegisterButton->setFont(font1);
-        RegisterButton->setStyleSheet(QString::fromUtf8("#RegistratiButton {\n"
-"color: #0033CC;\n"
+        RegisterButton->setGeometry(QRect(580, 80, 101, 31));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("Calibri"));
+        font2.setPointSize(16);
+        font2.setBold(true);
+        font2.setWeight(75);
+        RegisterButton->setFont(font2);
+        RegisterButton->setStyleSheet(QString::fromUtf8("#RegisterButton {\n"
+"color: #FFFFFF;\n"
 "background-color: Transparent;\n"
 "background-repeat:no-repeat;\n"
 "}\n"
 "\n"
-"#RegistratiButton:hover {\n"
-"color: #0040FF;\n"
+"#RegisterButton:hover {\n"
+"color: #FFFFFF;\n"
 "text-decoration: underline;\n"
 "}"));
         ForgotPasswordButton = new QPushButton(loginPage);
         ForgotPasswordButton->setObjectName(QString::fromUtf8("ForgotPasswordButton"));
-        ForgotPasswordButton->setGeometry(QRect(640, 360, 181, 21));
-        QFont font2;
-        font2.setPointSize(10);
-        ForgotPasswordButton->setFont(font2);
+        ForgotPasswordButton->setGeometry(QRect(750, 380, 151, 21));
+        QFont font3;
+        font3.setPointSize(10);
+        ForgotPasswordButton->setFont(font3);
         ForgotPasswordButton->setStyleSheet(QString::fromUtf8("#ForgotPasswordButton {\n"
 "color: #505050;\n"
 "background-color: Transparent;\n"
@@ -149,46 +148,43 @@ public:
 "}"));
         LoginUsernameForm = new QLineEdit(loginPage);
         LoginUsernameForm->setObjectName(QString::fromUtf8("LoginUsernameForm"));
-        LoginUsernameForm->setGeometry(QRect(641, 210, 231, 31));
+        LoginUsernameForm->setGeometry(QRect(784, 260, 231, 31));
+        LoginUsernameForm->setFont(font1);
         LoginUsernameForm->setStyleSheet(QString::fromUtf8("background-color: transparent;\n"
 "border-radius: 15px;\n"
 "padding: 0 8px;\n"
 "selection-background-color: darkgray;\n"
-"font-size: 16px;\n"
+"font-size: 18px;\n"
 ""));
         label_status = new QLabel(loginPage);
         label_status->setObjectName(QString::fromUtf8("label_status"));
-        label_status->setGeometry(QRect(10, 487, 81, 16));
+        label_status->setGeometry(QRect(10, 604, 81, 16));
         label_status->setStyleSheet(QString::fromUtf8("color: #EAFF00;"));
         stackedWidget->addWidget(loginPage);
         regPage = new QWidget();
         regPage->setObjectName(QString::fromUtf8("regPage"));
         regPage->setStyleSheet(QString::fromUtf8("#regPage {border-image: url(:/image/registrazione.png);}"));
-        Haigiaaccount = new QLabel(regPage);
-        Haigiaaccount->setObjectName(QString::fromUtf8("Haigiaaccount"));
-        Haigiaaccount->setGeometry(QRect(610, 480, 151, 21));
-        Haigiaaccount->setFont(font1);
         RegUsernameForm = new QLineEdit(regPage);
         RegUsernameForm->setObjectName(QString::fromUtf8("RegUsernameForm"));
-        RegUsernameForm->setGeometry(QRect(641, 210, 231, 31));
+        RegUsernameForm->setGeometry(QRect(784, 260, 231, 31));
         RegUsernameForm->setStyleSheet(QString::fromUtf8("background-color:transparent;\n"
 "border: transparent;\n"
 "padding: 0 8px;\n"
 "selection-background-color: darkgray;\n"
-"font-size: 16px;\n"
+"font-size: 18px;\n"
 ""));
         RegMailForm = new QLineEdit(regPage);
         RegMailForm->setObjectName(QString::fromUtf8("RegMailForm"));
-        RegMailForm->setGeometry(QRect(641, 307, 231, 31));
+        RegMailForm->setGeometry(QRect(784, 377, 231, 31));
         RegMailForm->setStyleSheet(QString::fromUtf8("background-color:transparent;\n"
 "border: transparent;\n"
 "padding: 0 8px;\n"
 "selection-background-color: darkgray;\n"
-"font-size: 16px;\n"
+"font-size: 18px;\n"
 ""));
         SignUpButton = new QPushButton(regPage);
         SignUpButton->setObjectName(QString::fromUtf8("SignUpButton"));
-        SignUpButton->setGeometry(QRect(740, 370, 121, 41));
+        SignUpButton->setGeometry(QRect(930, 430, 121, 41));
         SignUpButton->setFont(font);
         SignUpButton->setCursor(QCursor(Qt::PointingHandCursor));
         SignUpButton->setStyleSheet(QString::fromUtf8("#SignUpButton{color: rgb(255, 255, 255); \n"
@@ -205,27 +201,26 @@ public:
 "}"));
         AccediButton = new QPushButton(regPage);
         AccediButton->setObjectName(QString::fromUtf8("AccediButton"));
-        AccediButton->setGeometry(QRect(760, 480, 81, 23));
-        AccediButton->setFont(font1);
+        AccediButton->setGeometry(QRect(600, 40, 81, 23));
+        AccediButton->setFont(font2);
         AccediButton->setCursor(QCursor(Qt::PointingHandCursor));
         AccediButton->setStyleSheet(QString::fromUtf8("#AccediButton {\n"
-"color: #0033CC;\n"
+"color: #FFFFFF;\n"
 "background-color: Transparent;\n"
 "background-repeat:no-repeat;\n"
 "}\n"
 "\n"
 "#AccediButton:hover {\n"
-"color: #0040FF;\n"
 "text-decoration: underline;\n"
 "}"));
         RegPasswordForm = new QLineEdit(regPage);
         RegPasswordForm->setObjectName(QString::fromUtf8("RegPasswordForm"));
-        RegPasswordForm->setGeometry(QRect(641, 258, 231, 31));
+        RegPasswordForm->setGeometry(QRect(784, 320, 231, 31));
         RegPasswordForm->setStyleSheet(QString::fromUtf8("background-color:transparent;\n"
 "border: transparent;\n"
 "padding: 0 8px;\n"
 "selection-background-color: darkgray;\n"
-"font-size: 16px;\n"
+"font-size: 18px;\n"
 ""));
         RegPasswordForm->setEchoMode(QLineEdit::Password);
         stackedWidget->addWidget(regPage);
@@ -234,7 +229,7 @@ public:
         menuPage->setStyleSheet(QString::fromUtf8("#menuPage {border-image: url(:/image/start.png);}"));
         LogoutButton = new QPushButton(menuPage);
         LogoutButton->setObjectName(QString::fromUtf8("LogoutButton"));
-        LogoutButton->setGeometry(QRect(670, 470, 131, 41));
+        LogoutButton->setGeometry(QRect(830, 590, 131, 41));
         LogoutButton->setMaximumSize(QSize(16777199, 16777215));
         LogoutButton->setFont(font);
         LogoutButton->setCursor(QCursor(Qt::PointingHandCursor));
@@ -253,7 +248,7 @@ public:
 "}"));
         openEditor = new QPushButton(menuPage);
         openEditor->setObjectName(QString::fromUtf8("openEditor"));
-        openEditor->setGeometry(QRect(600, 290, 131, 31));
+        openEditor->setGeometry(QRect(720, 350, 131, 31));
         openEditor->setStyleSheet(QString::fromUtf8("#openEditor{\n"
 "	text-align: left;\n"
 "	color: #0000FF;\n"
@@ -271,7 +266,7 @@ public:
         openEditor->setIconSize(QSize(13, 13));
         Username = new QPushButton(menuPage);
         Username->setObjectName(QString::fromUtf8("Username"));
-        Username->setGeometry(QRect(670, 200, 211, 23));
+        Username->setGeometry(QRect(820, 250, 211, 23));
         Username->setStyleSheet(QString::fromUtf8("#Username{\n"
 "	text-align: left;\n"
 "	color: #0000FF;\n"
@@ -286,7 +281,7 @@ public:
         stackedWidget->addWidget(menuPage);
         exitButton = new QPushButton(centralwidget);
         exitButton->setObjectName(QString::fromUtf8("exitButton"));
-        exitButton->setGeometry(QRect(870, 0, 29, 23));
+        exitButton->setGeometry(QRect(1068, 2, 30, 24));
         exitButton->setCursor(QCursor(Qt::PointingHandCursor));
         exitButton->setStyleSheet(QString::fromUtf8("background-color: Transparent;\n"
 "background-repeat:no-repeat;\n"
@@ -294,10 +289,10 @@ public:
         exitButton->setIconSize(QSize(25, 25));
         version = new QLabel(centralwidget);
         version->setObjectName(QString::fromUtf8("version"));
-        version->setGeometry(QRect(10, 503, 191, 16));
-        QFont font3;
-        font3.setItalic(true);
-        version->setFont(font3);
+        version->setGeometry(QRect(10, 620, 191, 16));
+        QFont font4;
+        font4.setItalic(true);
+        version->setFont(font4);
         version->setStyleSheet(QString::fromUtf8("color: #EAFF00;"));
         StartWindow->setCentralWidget(centralwidget);
         QWidget::setTabOrder(RegUsernameForm, RegPasswordForm);
@@ -324,23 +319,21 @@ public:
     {
         StartWindow->setWindowTitle(QCoreApplication::translate("StartWindow", "MainWindow", nullptr));
         LoginAdmin->setText(QCoreApplication::translate("StartWindow", "Accedi (Admin Mode)", nullptr));
-        Seinuovo->setText(QCoreApplication::translate("StartWindow", "Sei nuovo?", nullptr));
         LoginButton->setText(QCoreApplication::translate("StartWindow", "Accedi", nullptr));
         LoginPasswordForm->setInputMask(QString());
         LoginPasswordForm->setText(QString());
         LoginPasswordForm->setPlaceholderText(QCoreApplication::translate("StartWindow", "Password", nullptr));
-        RegisterButton->setText(QCoreApplication::translate("StartWindow", "Registrati", nullptr));
+        RegisterButton->setText(QCoreApplication::translate("StartWindow", "REGISTRATI", nullptr));
         ForgotPasswordButton->setText(QCoreApplication::translate("StartWindow", "Password dimenticata?", nullptr));
         LoginUsernameForm->setText(QString());
         LoginUsernameForm->setPlaceholderText(QCoreApplication::translate("StartWindow", "Username", nullptr));
         label_status->setText(QCoreApplication::translate("StartWindow", "Status", nullptr));
-        Haigiaaccount->setText(QCoreApplication::translate("StartWindow", "Hai gi\303\240 un account?", nullptr));
         RegUsernameForm->setText(QString());
         RegUsernameForm->setPlaceholderText(QCoreApplication::translate("StartWindow", "Username", nullptr));
         RegMailForm->setText(QString());
         RegMailForm->setPlaceholderText(QCoreApplication::translate("StartWindow", "Email", nullptr));
         SignUpButton->setText(QCoreApplication::translate("StartWindow", "Registrati", nullptr));
-        AccediButton->setText(QCoreApplication::translate("StartWindow", "Accedi", nullptr));
+        AccediButton->setText(QCoreApplication::translate("StartWindow", "ACCEDI", nullptr));
         RegPasswordForm->setInputMask(QString());
         RegPasswordForm->setText(QString());
         RegPasswordForm->setPlaceholderText(QCoreApplication::translate("StartWindow", "Password", nullptr));
