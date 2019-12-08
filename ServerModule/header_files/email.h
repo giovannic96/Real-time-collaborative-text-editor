a0191e3f8ac7d580157870cc6ba44994e3e6479e
giovannic96 <giovanni.cala@hotmail.it>
2019-12-08 11:02:07 +0100

    fixed db and change version

--- ServerModule/header_files/email.h
+++ ServerModule/header_files/email.h
@@ -13,17 +13,17 @@
 #include <sstream>
 
 static const char *payload_text[] = {
-        //"Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n", //this will be substituted with the current date
-        "To: \r\n", //this will be substituted with the correct email
-        "From: Carte Editor\r\n",
-        //"Message-ID: <dcd7cb36-11db-487a-9f3a-e652a9458efk@" "rfcpedant.example.org>\r\n",//leave this line commented because the
-        "Subject: Sei stato invitato a collaborare\r\n",
-        "\r\n", /* empty line to divide headers from body, see RFC5322 */
-        "The body of the message starts here.\r\n", //this will be substituted with the URI of the file
-        "\r\n",
-        "Buon lavoro!\r\n",
-        "TEAM C.A.R.T.E.\r\n",
-        nullptr
+    //"Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n", //this will be substituted with the current date
+    "To: \r\n", //this will be substituted with the correct email
+    "From: Carte Editor\r\n",
+    //"Message-ID: <dcd7cb36-11db-487a-9f3a-e652a9458efk@" "rfcpedant.example.org>\r\n",//leave this line commented because the
+    "Subject: Sei stato invitato a collaborare\r\n",
+    "\r\n", /* empty line to divide headers from body, see RFC5322 */
+    "The body of the message starts here.\r\n", //this will be substituted with the URI of the file
+    "\r\n",
+    "Buon lavoro!\r\n",
+    "TEAM C.A.R.T.E.\r\n",
+    nullptr
 };
 
 struct upload_status {
