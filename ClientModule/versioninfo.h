c6240870f4dbc1604722af366b2e3b38bd33f584
Enrico Mancuso <55850895+HidroSaphire@users.noreply.github.com>
2020-04-01 23:52:24 +0200

    NEW BETA VERSION --> v0.4.0b

--- ClientModule/versioninfo.h
+++ ClientModule/versioninfo.h
@@ -13,7 +13,7 @@ private:
     QString str;
 public:
     //CONSTRUCTOR WITHOUT PARAMETER (Default)
-    VersionInfo(): Major(0), Minor(3), Patch(3), ReleaseType("a"), BuildTypeAndNumber("Canary 1385") {
+    VersionInfo(): Major(0), Minor(4), Patch(0), ReleaseType("b"), BuildTypeAndNumber("Canary 1400") {
         str = "v"+QString::number(Major)+"."+QString::number(Minor)+"."+QString::number(Patch)+ReleaseType+" - "+BuildTypeAndNumber;
     }
 
